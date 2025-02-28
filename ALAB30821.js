94277d82947ebded6443418e51cd82cc27a64880
Anisa Monger <linnus2004@gmail.com>
2025-02-27 22:59:56 -0500

    "WEEK 2 AND 3 ADDED TO PART 1"

--- ALAB30821.js
+++ ALAB30821.js
@@ -1,31 +1,69 @@
 // Part 1 Prediction of Plant growth after a week
+// Week 1
 const PI = 3.1415
 let radius = 5
 const area = PI * radius * radius
 
 let plants = 20
-let min_per_plant = 0.8
+let minSpacePerPlant = 0.8
 let capacity = 0
 
-capacity = (plants * 2) * min_per_plant
-plants = plants * 2
-console.log("The number of plants for week 1 is " + plants)
+
+// plants = plants * 2
+// capacity = plants * minSpacePerPlant
+// console.log("The number of plants for week 1 is " + plants)
+
+// console.log("The capacity is " + capacity)
+
+// if (capacity > area * 0.8) {
+//   console.log("Week 1 maximum capacity is over 80%. Garden needs to be pruned.")
+
+// } else if(capacity >= area * 0.5) {
+// console.log("Week 1 maximum capacity is between 50% to 80%. Garden needs to be monitored.")
+
+// } else {
+//   console.log("week 1 maximum capacity is less than 50%. There is room to plant more.")
+// }
+
+// Part 1 Week 2
+// plants = (plants * 2) * 2
+// capacity = plants * minSpacePerPlant
+// console.log("The number of plants for week 2 is " + plants)
+
+// console.log("The capacity is " + capacity)
+
+// if (capacity > area * 0.8) {
+//   console.log("Week 2 maximum capacity is over 80%. Garden needs to be pruned.")
+
+// } else if(capacity >= area * 0.5) {
+// console.log("Week 2 maximum capacity is between 50% to 80%. Garden needs to be monitored.")
+
+// } else {
+//   console.log("week 2 maximum capacity is less than 50%. There is room to plant more.")
+// }
+
+// Part 1 Week 3
+plants = (plants * 2) * (2 * 2)
+capacity = plants * minSpacePerPlant
+console.log("The number of plants for week 3 is " + plants)
 
 console.log("The capacity is " + capacity)
 
 if (capacity > area * 0.8) {
-  console.log("Week 1 maximum capacity is over 80%. Garden needs to be pruned.")
+  console.log("Week 3 maximum capacity is over 80%. Garden needs to be pruned.")
 
 } else if(capacity >= area * 0.5) {
-console.log("Week 1 maximum capacity is between 50% to 80%. Garden needs to be monitored.")
+console.log("Week 3 maximum capacity is between 50% to 80%. Garden needs to be monitored.")
 
 } else {
-  console.log("week 1 maximum capacity is less than 50%. There is room to plant more.")
+  console.log("week 3 maximum capacity is less than 50%. There is room to plant more.")
 }
-
 // Part 2
 
 
+
+
+// Part 3
 
 
 // let growth = 2
